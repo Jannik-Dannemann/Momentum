0e5766523ba8d92e631236575eea6bedb2e17be1
Jannik Dannemann <jannik.gaming9@gmail.com>
2025-10-13 14:36:09 +0300

    auto

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- output: 'export',
+  output: 'export',            // needed for next export
+  basePath: "/Momentum",       // just your repo name, with a leading slash
+  assetPrefix: "/Momentum/",   // same here, ensure CSS/JS load correctly
 };
 
 export default nextConfig;
